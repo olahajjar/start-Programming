85435c709b3032b6652216b58c1b6c28c6855ae0
olahajjar <ola.star2008@hotmail.com>
2017-08-19 17:01:40 +0300

    tabel2

--- start Programming/Controllers/ListNotesTableViewController.swift
+++ start Programming/Controllers/ListNotesTableViewController.swift
@@ -6,34 +6,27 @@
 //  Copyright © 2017 Makeschool. All rights reserved.
 //
 
-import Foundation
+
 import UIKit
 class ListNotesTableViewController: UITableViewController {
+    let names:[String]=["java","javaScript","swift","C","C++","Html","Jqury","php","css","angular"]
     override func viewDidLoad() {
     super.viewDidLoad()
     }
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // 1
-        if let identifier = segue.identifier {
-            // 2
-            if identifier == "displayNote" {
-                // 3
-                print("Transitioning to the Display Note View Controller")
-            }
-        }
-    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return names.count
     }
     
     // 2
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // 1
         let cell = tableView.dequeueReusableCell(withIdentifier: "listNotesTableViewCell", for: indexPath) as! ListNotesTableViewCell
         
-        // 2
-        cell.programmingLanguge.text = "hey"
+        // 2c
         
+        cell.programmingLanguge.text = names[indexPath.row]
         
         return cell
     }
--- start Programming/Views/ListNotesTableViewCell.swift
+++ start Programming/Views/ListNotesTableViewCell.swift
@@ -1,15 +1,15 @@
 //
-//   ListNotesTableViewCell.swift
+//  ListNotesTableViewCell.swift
 //  start Programming
 //
-//  Created by make on 8/18/17.
+//  Created by make on 8/19/17.
 //  Copyright © 2017 Makeschool. All rights reserved.
 //
 
+
 import UIKit
-class ListNotesTableViewCell: UITableViewCell {
+class ListNotesTableViewCell: UITableViewCell{
     
     @IBOutlet weak var programmingLanguge: UILabel!
     
-    }
-
+}
